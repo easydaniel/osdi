dcaebd3be000a912df66ac8538a273967ba25461
0316222/tachien <whitetiger50512@gmail.com>
2017-03-02 20:50:46 +0800

    update debug.sh

--- lab1/debug.sh
+++ lab1/debug.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-qemu-system-i386 -m 16M -boot order=a,menu=on,splash=../lab2/bootsplash.jpg,splash-time=10000 -fda linux-0.11/Image -hda osdi.img -bios bios.bin -display vnc=0.0.0.0:0 -chardev pipe,path=qemudebugpipe,id=seabios -device isa-debugcon,iobase=0x402,chardev=seabios 
+qemu-system-i386 -m 16M -boot order=a,menu=on,splash=../lab2/bootsplash.jpg,splash-time=10000 -fda linux-0.11/Image -hda osdi.img -bios bios.bin -display sdl -chardev pipe,path=qemudebugpipe,id=seabios -device isa-debugcon,iobase=0x402,chardev=seabios 
